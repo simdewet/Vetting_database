ce0441a9ef8d9d4ee4e06a0ad729641270c2fa2d
simdewet <simdewet@gmail.com>
2020-04-07 15:59:56 +0200

    Added image modal in candidates table

--- candidates/table.py
+++ candidates/table.py
@@ -15,8 +15,8 @@ class CandidatesTable(tables.Table):
 	scorr_1 = tables.Column(accessor="Transient_file.scorr_1")
 	scorr_2 = tables.Column(accessor="Transient_file.scorr_2")
 	external_links = tables.TemplateColumn('{% if record.simbad %} <a href="{{record.simbad}}" target="_blank">Simbad</a> {% endif %} {% if record.gaia %} <a href="{{record.gaia}}" target="_blank">Vizier_gaia</a> {% endif %} {% if record.TNS %} <a href="{{record.TNS}}" target="_blank">TNS</a> {% endif %}')
-	image_series = tables.TemplateColumn('{% if record.image_series %} <a href="{{record.image_series.url}}" target="_blank"> <img src="{{record.image_series.url}}"" width="50px"> </a>{% endif %}')
-	light_curves = tables.TemplateColumn('{% if record.lightcurve %} <a href="{{record.lightcurve.url}}" target="_blank"> <img src="{{record.lightcurve.url}}"" width="100px"> </a>{% endif %}')
+	image_series = tables.TemplateColumn('{% extends "candidates/image_modal.html" %}{% block content %}{% if record.image_series %} <a href="javascript:void(0)"> <img class="img-responsive" src="{{record.image_series.url}}" width="50px"> </a>{% endif %}{% endblock %}')
+	light_curves = tables.TemplateColumn('{% extends "candidates/image_modal.html" %}{% block content %}{% if record.lightcurve %} <a href="javascript:void(0)"> <img class="img-responsive" src="{{record.lightcurve.url}}" width="100px"> </a>{% endif %}{% endblock %}')
 
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
